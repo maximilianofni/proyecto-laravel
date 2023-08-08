2c2a3c651b1cc831347ab453d1f06e42e896d4c4
maximilianofni <maximilianofni@gmail.com>
2023-08-07 22:08:00 -0300

    se agrego el registro y login con breeze y se agrego el register y login con google

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,55 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
-use App\Http\Controllers\Auth\LoginController;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*
+login con google
+*/
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->stateless()->user();
+    
+    $user = User::updateOrCreate([
+        'google_id' => $user_google->id,
+    ],[
+        'name' => $user_google->name, 
+        'email' => $user_google->email,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+ 
+});
 
 
 
-route::view('/', 'welcome');
-route::view('login', 'login');
-route::view('dashboard', 'dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-route::post('login', [LoginController::class, 'login']);
-route::post('logout', [LoginController::class, 'logout']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
